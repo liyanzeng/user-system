cc1a658cf283a5c419eb6159584f267a660b1ed6
yanzeng li <yanzengli@gmail.com>
2016-09-29 10:44:22 +0800

    modify Read Me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929013859) do
+ActiveRecord::Schema.define(version: 20160929022401) do
+
+  create_table "access_tokens", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "token"
+    t.datetime "expire_in"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
