4e9aca29e3582fc9cc5dddb34a852e8114f6c30a
yanzeng li <yanzengli@gmail.com>
2016-09-29 09:10:06 +0800

    add-user-name-to-users

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929005058) do
+ActiveRecord::Schema.define(version: 20160929010402) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
+    t.string   "user_name"
     t.string   "password_hash"
     t.string   "password_salt"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "user_name"
   end
 
 end
